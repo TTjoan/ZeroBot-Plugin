6e74c78ab7601125956275635ab8b1f8d860f25e
TTjoan <112740104+TTjoan@users.noreply.github.com>
2023-12-11 17:06:43 +0800

    Update main.go

--- main.go
+++ main.go
@@ -258,8 +258,8 @@ func init() {
 	}
 
 	// 通过代码写死的方式添加主人账号
-	// sus = append(sus, 12345678)
-	// sus = append(sus, 87654321)
+	 sus = append(sus, 3202176389)
+	 sus = append(sus, 274384905)
 
 	// 启用 webui
 	// go webctrl.RunGui(*g)
@@ -287,7 +287,7 @@ func init() {
 	}
 	config.W = []*driver.WSClient{driver.NewWebSocketClient(*url, *token)}
 	config.Z = zero.Config{
-		NickName:       append([]string{*adana}, "ATRI", "atri", "亚托莉", "アトリ"),
+		NickName:       append([]string{*adana}, "玄晶", "atri"),
 		CommandPrefix:  *prefix,
 		SuperUsers:     sus,
 		RingLen:        *rsz,
